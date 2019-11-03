58a7282195a2118708c6c1bae5c6ba929abb11cc
Paolo Di Giglio <p.digiglio91@gmail.com>
2019-11-03 15:12:32 +0100

    Clean-up; +make_length()

--- Length.h
+++ Length.h
@@ -9,33 +9,51 @@
 namespace observable
 {
 
+/// @brief A length observable.
+/// @tparam Representation The underlying length type.
+/// @tparam Ratio The ratio (w.r.t. meter).
 template <typename Representation, typename Ratio = traits::unit_ratio>
 class Length final
 {
 public:
     using representation_type = Representation;
 
-    explicit Length(Representation value)
+    /// @brief Constructor.
+    /// @param value The length value.
+    constexpr explicit Length(Representation value) noexcept 
         :
             Value_(value)
     { }
 
+    /// @brief Copy constructor.
     Length(const Length&) = default;
+    /// @brief Move constructor.
     Length(Length&&) = default;
 
+    /// @brief Copy assignment operator.
     Length& operator=(const Length&) = default;
+    /// @brief Copy assignment operator.
     Length& operator=(Length&&) = default;
 
+    /// @brief Destructor.
     ~Length() = default;
 
-    representation_type value() const
+    /// @brief Get the underlying value.
+    constexpr representation_type value() const
     {
         return this->Value_;
     }
 
 private:
+    /// @brief the length value.
     Representation Value_;
 };
+
+template <typename Ratio, typename Representation>
+constexpr auto make_length(Representation value) noexcept 
+{
+    return Length<Representation, Ratio>(value);
+}
 
 template <typename Representation, typename Ratio>
 std::string to_string(const Length<Representation, Ratio>& l)
@@ -50,6 +68,12 @@ std::ostream& operator<<(std::ostream& os, const Length<Representation, Ratio>& 
     return os << to_string(l);
 }
 
+template <typename ToRatio, typename FromRatio = traits::unit_ratio>
+constexpr auto conversion_factor() noexcept
+{
+    return ratio_evaluate<std::ratio_divide<FromRatio, ToRatio>>();
+}
+
 template <
     typename ToRepresentation,
     typename ToRatio,
@@ -62,6 +86,11 @@ constexpr auto length_cast(const Length<FromRepresentation, FromRatio>& from) no
     return Length<ToRepresentation, ToRatio>(v);
 }
 
+/// @brief Cast a length to a different unit of measure.
+/// @param from The length to cast from.
+/// @tparam ToRatio The ratio to cast `from` to.
+/// @tparam FromRepresentation The representation type of `from`.
+/// @tparam FromRatio The ratio type of `from`.
 template <
     typename ToRatio,
     typename FromRepresentation,
--- ratio_utils.h
+++ ratio_utils.h
@@ -2,21 +2,24 @@
 
 #include "traits_utils.h"
 
+/// @brief Evaluate a ratio without casting its numerator and denominator.
+/// @return The evaluated ratio (type: `intmax_t`).
 template <typename Ratio>
 constexpr auto ratio_evaluate_impl(std::true_type) noexcept { return Ratio::num / Ratio::den; }
 
+/// @brief Evaluate a ratio casting its numerator to `double`.
+/// @return The evaluated ratio (type: `double`).
 template <typename Ratio>
 constexpr auto ratio_evaluate_impl(std::false_type) noexcept { return static_cast<double>(Ratio::num) / Ratio::den; }
 
+/// @brief Evaluate a ratio.
+/// @tparam Ratio The ratio to evaluate.
+/// @return The value of `Ratio::num / Ratio::den` (type: either `double` or `intmax_t`).
 template <typename Ratio>
 constexpr auto ratio_evaluate() noexcept { return ratio_evaluate_impl<Ratio>(traits::ratio_integer_t<Ratio>{}); }
 
-template <typename ToRatio, typename FromRatio = traits::unit_ratio>
-constexpr auto conversion_factor() noexcept
-{
-    return ratio_evaluate<std::ratio_divide<FromRatio, ToRatio>>();
-}
-
+/// @brief Get the unit prefix.
+/// @tparam Ratio The ratio whose prefix to get.
 template <typename Ratio>
 std::string get_unit_prefix_string()
 {
@@ -24,12 +27,18 @@ std::string get_unit_prefix_string()
     return "(" + to_string(Ratio::num) + "/" + to_string(Ratio::den) + ")";
 }
 
+/// @brief Get the unit prefix (`std::milli` specialization).
+/// @tparam Ratio The ratio whose prefix to get.
+/// @return "m".
 template <>
 std::string get_unit_prefix_string<std::milli>()
 {
     return "m";
 }
 
+/// @brief Get the unit prefix (`std::centi` specialization).
+/// @tparam Ratio The ratio whose prefix to get.
+/// @return "c".
 template <>
 std::string get_unit_prefix_string<std::centi>()
 {
